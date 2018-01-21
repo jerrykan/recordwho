b38489978efdf5ce26857947d0406f3291166e19
John Kristensen <john@jerrykan.com>
2018-01-22 00:08:26 +1100

    Cleanup and restructure config file for use in 2018

--- config.py
+++ config.py
@@ -1,28 +1,23 @@
 import logging
 import os
 
+### RecordWho Settings ###
+
 IRC_HOST = 'irc.freenode.net'
 IRC_NICK = 'lcawhobot'
 IRC_CHANS = (
-    '#taslugdev',
     '#linux.conf.au',
-    '#lca2017_plenary', '#lca2017_tasman_a', '#lca2017_tasman_bc',
-    '#lca2017_boardwalk', '#lca2017_wellington_1', '#lca2017_wellington_2',
 )
 WHO_CHANS = (
     '#linux.conf.au',
-    '#lca2017_plenary', '#lca2017_tasman_a', '#lca2017_tasman_bc',
-    '#lca2017_boardwalk', '#lca2017_wellington_1', '#lca2017_wellington_2',
 )
+IRC_ADMINS = ('jerrykan!~jerrykan@whatsit.theintraweb.net', )
 
-# This is a minimal configuration to get you started with the Text mode.
-# If you want to connect Errbot to chat services, checkout
-# the options in the more complete config-template.py from here:
-# https://raw.githubusercontent.com/errbotio/errbot/master/errbot/config-template.py
+### Errbot Configuration ###
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-BACKEND = 'IRC'  # Errbot will start in text mode (console only mode) and will answer commands from there.
+BACKEND = 'IRC'
 
 BOT_DATA_DIR = os.path.join(BASE_DIR, 'data')
 BOT_EXTRA_PLUGIN_DIR = os.path.join(BASE_DIR, 'plugins')
@@ -30,7 +25,7 @@ BOT_EXTRA_PLUGIN_DIR = os.path.join(BASE_DIR, 'plugins')
 BOT_LOG_FILE = os.path.join(BASE_DIR, 'errbot.log')
 BOT_LOG_LEVEL = logging.WARNING
 
-BOT_ADMINS = ('jerrykan!~jerrykan@whatsit.theintraweb.net', )
+BOT_ADMINS = IRC_ADMINS
 
 BOT_IDENTITY = {
     'server': IRC_HOST,
