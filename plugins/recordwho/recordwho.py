ef0435e0374337ea7f5ad0f9a472d93f68175f71
John Kristensen <john@jerrykan.com>
2018-01-22 00:23:19 +1100

    Round timestamp of json output file to nearest interval
    By rounding to the nearest interval we should have nicer looking time
    values when they are charted.

--- plugins/recordwho/recordwho.py
+++ plugins/recordwho/recordwho.py
@@ -23,9 +23,13 @@ class RecordWho(BotPlugin):
         self._who_list[event.arguments[0]].append(event.arguments)
 
     def do_endofwho(self, connection, event):
+        # round to nearest interval
+        t = int(time()) + (self.bot_config.RECORDWHO_INTERVAL // 2)
+        timestamp = t - (t % self.bot_config.RECORDWHO_INTERVAL)
+
         chan = event.arguments[0]
         fname = os.path.join(self.bot_config.RECORDWHO_DATA_DIR,
-                             '{}_{}.json'.format(chan, int(time())))
+                             '{}_{}.json'.format(chan, timestamp))
 
         with open(fname, 'w') as fh:
             json.dump(self._who_list[chan], fh)
